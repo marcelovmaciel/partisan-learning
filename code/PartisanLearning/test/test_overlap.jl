78b7160aef58b430ae5d10f13dff6a82e46ac3f5
marcelovmaciel <marcelovmaciel@protonmail.com>
2022-03-21 20:33:53 -0700

    better visualization

--- code/PartisanLearning/test/test_overlap.jl
+++ code/PartisanLearning/test/test_overlap.jl
@@ -88,46 +88,36 @@ function summaries_prop_voted_against(df)
 end
 
 
-function saveplots_collect_stats(whichdist,scattername)
+function collect_prop_voted_against(whichdist)
 
-    holderdf = DataFrame([Float64[], Float64[], Float64[]], [:min, :max, :mean])
+    holderdf = DataFrame([Int64[],
+                          Float64[],
+                          Int64[]],
+                         [:step,
+                          :get_prop_voted_against,
+                          :iter])
     for i in 1:30
         df = get_data_initial_dist(whichdist)
+        df[:iter] = [i for _ in 1:21]
+
         holderdf = vcat(holderdf,
-                        summaries_prop_voted_against(df))
-        plot_scatter(df, scattername, i)
+                        df)
+        #plot_scatter(df, scattername, i)
     end
 
     return(holderdf)
 end
 
-
-overlap50df = saveplots_collect_stats(pla.standard_1d_poss, "overlap50")
-
-overlap20df = saveplots_collect_stats(pla.overlap_20_poss, "overlap20")
-overlap80df = saveplots_collect_stats(pla.overlap_80_poss, "overlap80")
+overlap20df = collect_prop_voted_against(pla.overlap_20_poss)
+overlap50df = collect_prop_voted_against(pla.standard_1d_poss)
+overlap80df = collect_prop_voted_against(pla.overlap_80_poss)
 
 
-# THIS IS WRONG
-# f2 = Figure()
-# ax = Axis(f2[1, 1],
-#           xlabel = "step",
-#           ylabel = "Prop Cross Party Vote")
 
-# foostep = Float64.(collect(1:30))
+f2 = Figure()
+ax = Axis(f2[1, 1], xlabel = "step",
+          ylabel = "Prop Cross Party Vote")
 
 
-# sc1 = scatter!(foostep,
-#          overlap80df.mean, color = :black)
-
-
-# sc2= scatter!(foostep,
-#          overlap80df.min, color = :blue)
-
-
-# sc3 = scatter!(foostep,
-#          overlap80df.max, color = :red)
-
-# Legend(f2[1, 2],
-#     [sc1, sc2, sc3],
-#     ["mean", "min", "max"])
+boxplot!(overlap80df.step,
+             overlap80df.get_prop_voted_against)
